86cf9c7ae8c76493804d2ff8cd869a22aac7a5ec
Fran <francorossi.visual@gmail.com>
2023-04-08 20:54:50 -0300

    implemented delete functionality and UI

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 
-function App() {
+export default function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
   // console.log('app feedback', feedback);
 
   return (
     <>
       <Header text={'Feedback UI'} />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
 }
-
-export default App;
